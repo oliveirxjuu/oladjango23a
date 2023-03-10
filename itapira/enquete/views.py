f488eccb10be8584bcb744bb9d9643e843339160
lfernandoetec <lfernandoetec@gmail.com>
2023-03-10 08:50:39 -0300

    criado funcao index

--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>Olha, se voce nao me ama</h1><h2>Caneta azul, azul caneta</h2>")
