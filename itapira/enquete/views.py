c84dc2b2b3054a529f267e34611856ac928aa0e1
lfernandoetec <lfernandoetec@gmail.com>
2023-03-31 09:38:10 -0300

    urls

--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Olha, se voce nao me ama</h1><h2>Caneta azul, azul caneta</h2>")
+
+def caneta(request):
+    return HttpResponse("<h1>Caneta Azul, Azul caneta...</h1>")
