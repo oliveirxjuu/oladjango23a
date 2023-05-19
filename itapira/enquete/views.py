96b89814eca48fe090e5c2b9164d8fdf14e7d5ac
lfernandoetec <lfernandoetec@gmail.com>
2023-05-19 09:00:56 -0300

    index

--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Questao
 
 # Create your views here.
 
+# explicar isso semana que vem
 def index(request):
-    return HttpResponse("<h1>Olha, se voce nao me ama</h1><h2>Caneta azul, azul caneta</h2>")
+    ultimas_questoes = Questao.objects.order_by("-data")[:5]
+    saida = ", ".join([q.pergunta for q in ultimas_questoes])
+    return HttpResponse(saida)
+
 
 def caneta(request):
     return HttpResponse("<h1>Caneta Azul, Azul caneta...</h1>")
