dda62efde0040b7e79e8e3e988446e24dc68ba71
lfernandoetec <lfernandoetec@gmail.com>
2023-05-19 08:24:35 -0300

    parametros de url

--- itapira/enquete/urls.py
+++ itapira/enquete/urls.py
@@ -5,4 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('felipe', views.caneta, name='caneta'),
+     # ex: /enquete/5/
+    path("<int:questao_id>/", views.detalhe, name="detalhe"),
+    # ex: /enquete/5/resultados/
+    path("<int:questao_id>/resultados/", views.resultados, name="resultados"),
+    # ex: /enquete/5/voto/
+    path("<int:questao_id>/voto/", views.voto, name="voto"),
 ]
--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -8,3 +8,16 @@ def index(request):
 
 def caneta(request):
     return HttpResponse("<h1>Caneta Azul, Azul caneta...</h1>")
+
+
+def detalhe(request, questao_id):
+    return HttpResponse("Voce esta olhando a questao %s." % questao_id)
+
+
+def resultados(request, questao_id):
+    response = "Voce esta olhando o resultado da questao %s."
+    return HttpResponse(response % questao_id)
+
+
+def voto(request, questao_id):
+    return HttpResponse("Voce esta votando na questao %s." % questao_id)
