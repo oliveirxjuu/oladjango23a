e196d1728f713b984fe24268eeedaf284e8dcc7f
lfernandoetec <lfernandoetec@gmail.com>
2023-06-02 09:06:58 -0300

    detalhe

--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Questao
 
 # Create your views here.
 
-# explicar isso semana que vem
 def index(request):
+    # selecionar na tabela questoes os ultimos 5 objetos cadastrados
     ultimas_questoes = Questao.objects.order_by("data")[:5]
-    # saida = ",".join([q.pergunta for q in ultimas_questoes])
-    # saida = ""
-    # for q in ultimas_questoes:
-    #     linha = f"<p>{q.pergunta}</p>"
-    #     saida = saida + linha
+    # criamos um dicionario em python
+    # (semelhante aos arryas associativos do php)
+    # (ou aos arryas objetos literais do javascript)
+    # onde passamos essa variavel para ser utilizada no template
     context = {'ultimas_questoes': ultimas_questoes}
+    # retornar a funcao render, passando como argumentos
+    # a requisicao, o template que sera utilizado
+    # e as variaveis de contexto que serao utilizadas dentro do template
     return render(request, 'enquete/index.html', context)
 
 
@@ -21,7 +24,11 @@ def caneta(request):
 
 
 def detalhe(request, questao_id):
-    return HttpResponse("Voce esta olhando a questao %s." % questao_id)
+    try:
+        questao = Questao.objects.get(pk=questao_id)
+    except Questao.DoesNotExist:
+        raise Http404("Questão não ecxisty")
+    return render(request, 'enquete/detalhe.html', {'questao': questao})
 
 
 def resultados(request, questao_id):
